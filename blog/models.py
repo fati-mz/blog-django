459c6fe334c905f27eb33e77ae3a17f4a8e97506
fati <fati.mansoory@gmail.com>
2023-10-18 11:09:15 +0330

    initial model setup

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,34 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.conf import settings
+from datetime import datetime    
 
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=127,
+                            validators=[MinLengthValidator(3, "Name must be greater than 2 characters")])
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=127,
+                            validators=[MinLengthValidator(3, "Name must be greater than 2 characters")])
+    
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255,
+                             validators=[MinLengthValidator(2, "Title must be greater than 2 characters")])
     content = models.TextField()
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+    #                            related_name='favs_users')
+    # image = models.BinaryField(null=True, editable=True)
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    counted_views = models.IntegerField(default=0)
+    status = models.BinaryField(default=False)
+    published_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
---
+++ home/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from datetime import datetime    
+
+class Contact(models.Model):
+    name= models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message= models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
