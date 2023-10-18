c88ded363d82e58713490e2dc9496b59bda53d06
fati <fati.mansoory@gmail.com>
2023-10-18 13:12:15 +0330

    personalise Admin Panel and fix blog.Model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy= 'created_at'
+    empty_value_display = '-empty-'
+    list_display = ('title' ,'counted_views','status','published_at','created_at')
+    list_filter = ('status',)
+    ordering= ['-created_at']
+    search_fields = ['title','content']
--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 from django.conf import settings
-from datetime import datetime    
-
+from datetime import datetime
 
 
 class Category(models.Model):
     name = models.CharField(max_length=127,
                             validators=[MinLengthValidator(3, "Name must be greater than 2 characters")])
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=127,
                             validators=[MinLengthValidator(3, "Name must be greater than 2 characters")])
-    
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255,
@@ -24,11 +29,11 @@ class Post(models.Model):
     # image = models.BinaryField(null=True, editable=True)
     # category = models.ForeignKey(Category, on_delete=models.CASCADE)
     # tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    counted_views = models.IntegerField(default=0)
-    status = models.BinaryField(default=False)
+    counted_views = models.IntegerField(null=True, default=0)
+    status = models.BooleanField(default=False,)
     published_at = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
-
+    def __str__(self):
+        return self.title
