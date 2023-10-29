03e1aa0a68759a7d2e7574841e318c3b33b512f7
fati mansouri <fati.mansoory@gmail.com>
2023-10-29 14:44:11 +0330

    fix

--- blog/views.py
+++ blog/views.py
@@ -6,25 +6,25 @@ from .models import Post
 def index(request):
     post = Post.objects.filter(published_at__lte=timezone.now())
     context = {'posts': post}
-    return render(request, 'blog\index.html', context)
+    return render(request, 'blog/index.html', context)
 
 
 def about(request):
     post = Post.objects.filter(status=1)
     context = {'posts': post}
-    return render(request, 'blog\About.html', context)
+    return render(request, 'blog/About.html', context)
 
 
 def contact(request):
     post = Post.objects.filter(status=1)
     context = {'posts': post}
-    return render(request, 'blog\contact.html', context)
+    return render(request, 'blog/contact.html', context)
 
 
 def blog_home(request):
     post = Post.objects.filter(status=1)
     context = {'posts': post}
-    return render(request, 'blog\Blog_home.html', context)
+    return render(request, 'blog/Blog_home.html', context)
 
 
 def blog_single(request,p_id):
@@ -35,4 +35,4 @@ def blog_single(request,p_id):
     post.save()
     context = {'post': post,'next_post': next_post, 'pre_post': pre_post}
     
-    return render(request, 'blog\Blog_single.html', context)
+    return render(request, 'blog/Blog_single.html', context)
