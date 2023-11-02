cb1c6711dfad73f3523ee28a350a3075a7e8d286
fati mansouri <fati.mansoory@gmail.com>
2023-11-02 19:36:20 +0330

    fix post-category and latest-post part

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,3 +1,22 @@
 from django import template
+from blog.models import Post
+from blog.models import Category
 
-register = template.library
+register = template.Library()
+
+
+@register.inclusion_tag('blog/Blog_latest_post.html')
+def latestposts(arg=5):
+    posts=Post.objects.filter(status=1).order_by('-published_at')[:arg]
+    return {'posts':posts}
+
+
+@register.inclusion_tag('blog/Blog_post_category.html')
+def postcategories():
+    posts=Post.objects.filter(status=1)
+    categories=Category.objects.all()
+    cat_dict={}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+
+    return {'categories':cat_dict}
