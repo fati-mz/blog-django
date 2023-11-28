bfc2a6c20d0e89a222847281685a40dc57315b64
fati mansouri <fati.mansoory@gmail.com>
2023-11-28 16:52:39 +0330

    add log-out and sign-up

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request,user)
-            return redirect('/')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
 
-    return render(request, 'accounts/login.html')
+        return render(request, 'accounts/login.html')
+    else:
+        return redirect('/')
 
 
+@login_required
 def logout_view(request):
-    return render(request, 'accounts/logout.html')
+    logout(request)
+    return redirect('/')
 
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('accounts:login')
+            else:
+                messages.add_message(request, messages.ERROR,
+                                     'your username or pssword format is wrong')
+
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, 'accounts/signup.html', context)
+    else:
+        return redirect('/')
+
